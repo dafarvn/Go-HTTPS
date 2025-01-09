ecd839ab71214437ef71e5655635f93105831702
DaFa Rizki Revansyah <154033199+dafarvn@users.noreply.github.com>
2025-01-09 18:30:08 +0700

    Add files via upload

---
+++ www/growtopia/server_data.php
@@ -0,0 +1,12 @@
+server|134.209.111.51
+port|17070
+loginurl|ya.rvnaa.my.id
+type|1
+#maint| MetaGrow Server will be undergoing Maintenance in a few moments. -- DaFaFlare
+
+beta_server|127.0.0.1
+beta_port|17091
+
+beta_type|1
+meta|DaFaFlare
+RTENDMARKERBS1001
